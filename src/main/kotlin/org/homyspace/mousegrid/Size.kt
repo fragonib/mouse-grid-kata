f275689a774ac2dce4f2947d6bb427a64065137b
Francisco González <fragonib@gmail.com>
2019-10-19 12:00:58 +0200

    :arrow_up: Add size validation

--- src/main/kotlin/org/homyspace/mousegrid/Size.kt
+++ src/main/kotlin/org/homyspace/mousegrid/Size.kt
@@ -1,3 +1,10 @@
 package org.homyspace.mousegrid
 
-data class Size(val width: Int, val height: Int)
+data class Size(val width: Int, val height: Int) {
+
+    init {
+        require(width > 0) { "'width' should be grater or equal than 0" }
+        require(height > 0) { "'height' should be grater or equal than 0" }
+    }
+
+}
